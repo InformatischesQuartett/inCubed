d9b1416fb5f76ec5f8f64055da29ecdf267ba893
Alexander Scheurer <mail@aspepex.net>
2015-07-21 10:54:34 +0200

    Intoduced GameStates

--- Assets/Scripts/Config.cs
+++ Assets/Scripts/Config.cs
@@ -1,3 +1,13 @@
-﻿public static class Config {
+﻿using System.Collections.Generic;
 
+public static class Config
+{
+    public static Dictionary<string, bool> GameStates { get; set; }
+
+    static Config()
+    {
+        //initialize GameStates
+        GameStates = new Dictionary<string, bool>();
+        GameStates.Add("gameStarted", false);
+    }
 }
