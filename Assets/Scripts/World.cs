cd61e1ffeb974989e6d41e32dbade35174b581e0
Alexander Scheurer <mail@aspepex.net>
2015-07-22 20:55:30 +0200

    Prefabs

--- Assets/Scripts/World.cs
+++ Assets/Scripts/World.cs
@@ -13,9 +13,29 @@ public class World : MonoBehaviour
 
         inputGamepad = GameObject.Find("CardboardMain").GetComponent<InputGamepad>();
 
-        sides[0] = Instantiate(Resources.Load("Prefabs/25kpoly") as GameObject);
+        sides[0] = Instantiate(Resources.Load("Prefabs/Gebirge") as GameObject);
         sides[0].transform.parent = this.transform;
-        sides[0].name = "25kpoly";
+        sides[0].name = "Gebirge";
+
+        sides[1] = Instantiate(Resources.Load("Prefabs/Dorf") as GameObject);
+        sides[1].transform.parent = this.transform;
+        sides[1].name = "Dorf";
+
+        sides[2] = Instantiate(Resources.Load("Prefabs/Eiswelt") as GameObject);
+        sides[2].transform.parent = this.transform;
+        sides[2].name = "Eiswelt";
+
+        sides[3] = Instantiate(Resources.Load("Prefabs/Feuer") as GameObject);
+        sides[3].transform.parent = this.transform;
+        sides[3].name = "Feuer";
+
+        sides[4] = Instantiate(Resources.Load("Prefabs/Wueste") as GameObject);
+        sides[4].transform.parent = this.transform;
+        sides[4].name = "Wueste";
+
+        sides[5] = Instantiate(Resources.Load("Prefabs/Wald") as GameObject);
+        sides[5].transform.parent = this.transform;
+        sides[5].name = "Wald";
 
         for (int i = 0; i < sides.Length; i++)
         {
@@ -28,11 +48,11 @@ public class World : MonoBehaviour
         }
 
         //Front Side
-        sides[0].transform.position = new Vector3(0, 0, 84);
+        sides[0].transform.position = new Vector3(0, 0, 100);
         sides[0].transform.Rotate(new Vector3(-90, 0, 0));
 
         //Back Side
-        sides[1].transform.position = new Vector3(0, 0, -92);
+        sides[1].transform.position = new Vector3(0, 0, -100);
         sides[1].transform.Rotate(new Vector3(90, 0, 0));
 
         //Right
@@ -44,7 +64,7 @@ public class World : MonoBehaviour
         sides[3].transform.Rotate(new Vector3(0, 0, -90));
 
         //Top
-        sides[4].transform.position = new Vector3(0, 97, 0);
+        sides[4].transform.position = new Vector3(0, 100, 0);
         sides[4].transform.Rotate(180, 0, 0);
 
         //Bottom
