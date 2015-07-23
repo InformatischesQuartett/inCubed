790c4d4b9b4b8f5fa7074f5fe8259f7898a2a3a1
Alexander Scheurer <mail@aspepex.net>
2015-07-23 21:34:16 +0200

    The first Quest is fully working + Audio

--- Assets/Scripts/EventController.cs
+++ Assets/Scripts/EventController.cs
@@ -1,17 +1,22 @@
 ﻿using UnityEngine;
 
-public class EventController : MonoBehaviour {
+public class EventController : MonoBehaviour
+{
+
+    public GameObject uicanvas;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+	{
+	    uicanvas = GameObject.Find("UICanvas");
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	    if (Input.GetKeyDown(KeyCode.L))
 	    {
-	        LetThereBeLight();
+	        FirstCube();
+
 	    }
 	}
 
@@ -22,5 +27,16 @@ public class EventController : MonoBehaviour {
         RenderSettings.defaultReflectionMode = UnityEngine.Rendering.DefaultReflectionMode.Skybox;
 
         GameObject.Find("frau_2").SendMessage("dubistneganzliebedrumwinkmalbitte");
+    }
+
+    void FirstCube()
+    {
+        uicanvas.SendMessage("Display", 2);
+        GameObject.Find("frau_2").GetComponent<AudioSource>().Play();
+    }
+
+    void GebirgeGrav()
+    {
+    GameObject.Find("kasten_gravinator_gebirge").GetComponent<AudioSource>().Play();
     }
 }
