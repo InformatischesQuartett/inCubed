b7e725a0218f48094f1e842a642ffb013518f7d5
Fabian Gärtner <fabian.gaertner@hs-furtwangen.de>
2015-07-23 18:34:33 +0200

    Outro (almost) Done

---
+++ Assets/Scripts/OutroBehavior.cs
@@ -0,0 +1,317 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityStandardAssets.ImageEffects;
+
+public class OutroBehavior : MonoBehaviour {
+    public enum OutroState
+    {
+        Island,
+        Transport1,
+        Wormhole,
+        Story,
+        StoryStay,
+        Transport2,
+        RealMode
+    }
+
+    // TODO: not public, use find and stuff
+    public World WorldScript;
+
+    public GameObject Plane;
+    public GameObject StoryPlane;
+    public GameObject Planes;
+    public Texture WormholeTex;
+    public RawImage WebcamTex;
+    public GameObject CalibRect;
+    public CardboardHead CardbordScript;
+
+    public Camera leftCam;
+    public Camera rightCam;
+
+    private OutroState _outroState;
+
+    private float _rotSpeed;
+    private float _starTime;
+    private int _storyPlaneId;
+    private Vector3 _tempTargetPos;
+
+    private float _lastDataCheck;
+    private Texture2D _webcamTex;
+
+    private Renderer _planeRenderer;
+    private Grayscale _grayScriptLeft;
+    private Twirl _twirlScriptLeft;
+    private ColorCorrectionCurves _cccScriptLeft;
+    private Grayscale _grayScriptRight;
+    private Twirl _twirlScriptRight;
+    private ColorCorrectionCurves _cccScriptRight;
+
+    private const float AngleIncrease = 3f;
+    private const float SaturationDecrease = 0.25f;
+    private const float RampIncrease = 0.25f;
+
+    private readonly Vector3 _planeStartPos = new Vector3(0f, 0f, -8.5f);
+    private readonly Vector3 _planeOddPos = new Vector3(-20f, 0f, -8.5f);
+    private readonly Vector3 _planeEvenPos = new Vector3(20f, 0f, -8.5f);
+    private readonly Vector3 _planeTargetSize = new Vector3(0.3f, 1, 0.169f);
+    private readonly Vector3 _planeTargetSizeMax = new Vector3(1.00f, 1, 0.562f);
+
+    private void Start()
+    {
+        _outroState = OutroState.Island;
+        _starTime = Time.realtimeSinceStartup;
+
+        _rotSpeed = 10;
+        _lastDataCheck = -1;
+        _storyPlaneId = 1;
+
+        //_webcamTex = new Texture2D(1920, 1080, TextureFormat.RGBA32, false);
+        _webcamTex = new Texture2D(1024, 768, TextureFormat.RGBA32, false);
+
+        Planes.SetActive(false);
+
+        _planeRenderer = Plane.GetComponent<Renderer>();
+
+        StoryPlane.transform.localScale = new Vector3(0.0001f, 0, 0.0001f);
+        _tempTargetPos = new Vector3(Random.Range(-0.01f, 0.01f), Random.Range(-0.01f, 0.01f), _planeStartPos.z);
+
+        var nextTex = Resources.Load<Texture2D>("Textures/Credits/Credits1");
+        StoryPlane.GetComponent<Renderer>().material.mainTexture = nextTex;
+
+        _cccScriptLeft = leftCam.GetComponent<ColorCorrectionCurves>();
+        _cccScriptRight = rightCam.GetComponent<ColorCorrectionCurves>();
+
+        _grayScriptLeft = leftCam.GetComponent<Grayscale>();
+        _grayScriptRight = rightCam.GetComponent<Grayscale>();
+        _grayScriptLeft.enabled = false;
+        _grayScriptRight.enabled = false;
+
+        _twirlScriptLeft = leftCam.GetComponent<Twirl>();
+        _twirlScriptRight = rightCam.GetComponent<Twirl>();
+        _twirlScriptLeft.enabled = false;
+        _twirlScriptRight.enabled = false;
+    }
+
+    private void Update()
+    {
+        if (!WorldScript.IsWorldOpening && !WorldScript.IsWorldOpen)
+            WorldScript.OpenWorld();
+
+        if (!WorldScript.IsWorldOpen)
+            return;
+
+        switch (_outroState)
+        {
+            case OutroState.Island:
+                if (Time.realtimeSinceStartup - _starTime > 10)
+                    _outroState = OutroState.Transport1;
+
+                break;
+
+            case OutroState.Transport1:
+                if (_cccScriptLeft.saturation >= 0)
+                {
+                    _cccScriptLeft.saturation -= SaturationDecrease*Time.deltaTime;
+                    _cccScriptRight.saturation -= SaturationDecrease*Time.deltaTime;
+                }
+                else
+                {
+                    if (!_grayScriptLeft.enabled)
+                    {
+                        _grayScriptLeft.enabled = true;
+                        _grayScriptRight.enabled = true;
+                    }
+
+                    _grayScriptLeft.rampOffset += RampIncrease*Time.deltaTime;
+                    _grayScriptRight.rampOffset += RampIncrease * Time.deltaTime;
+                }
+
+                if (_grayScriptLeft.rampOffset > 1.0f)
+                {
+                    Planes.SetActive(true);
+                    _planeRenderer.material.mainTexture = WormholeTex;
+                    _outroState = OutroState.Wormhole;
+                }
+
+                break;
+
+            case OutroState.Wormhole:
+                CardbordScript.trackPosition = false;
+                CardbordScript.trackRotation = false;
+
+                Plane.transform.Rotate(new Vector3(0, 1, 0), _rotSpeed * Time.deltaTime);
+
+                if (_grayScriptLeft.rampOffset >= 0)
+                {
+                    _grayScriptLeft.rampOffset -= RampIncrease * Time.deltaTime;
+                    _grayScriptRight.rampOffset -= RampIncrease * Time.deltaTime;
+                }
+                else
+                {
+                    if (_grayScriptLeft.enabled)
+                    {
+                        _grayScriptLeft.enabled = false;
+                        _grayScriptRight.enabled = false;
+                    }
+
+                    if (_cccScriptLeft.saturation < 0.4f)
+                    {
+                        _cccScriptLeft.saturation += SaturationDecrease * Time.deltaTime;
+                        _cccScriptRight.saturation += SaturationDecrease * Time.deltaTime;
+                    }
+                    else
+                    {
+                        _cccScriptLeft.saturation = 0.45f;
+                        _cccScriptRight.saturation = 0.45f;
+                    }
+                }
+
+                if (_cccScriptLeft.saturation > 0.4f)
+                    _outroState = OutroState.Story;
+
+                break;
+
+            case OutroState.Story:
+                Plane.transform.Rotate(new Vector3(0, 1, 0), _rotSpeed * Time.deltaTime);
+
+                StoryPlane.transform.localScale = Vector3.Lerp(StoryPlane.transform.localScale,
+                    _planeTargetSize, Time.deltaTime);
+
+                if (StoryPlane.transform.localScale.x >= 0.14f)
+                {
+                    _starTime = Time.realtimeSinceStartup;
+                    _outroState = OutroState.StoryStay;
+                }
+
+                break;
+
+            case OutroState.StoryStay:
+                Plane.transform.Rotate(new Vector3(0, 1, 0), _rotSpeed * Time.deltaTime);
+
+                if (Time.realtimeSinceStartup - _starTime > 5)
+                {
+                    StoryPlane.transform.localScale = Vector3.Lerp(StoryPlane.transform.localScale,
+                        _planeTargetSizeMax, Time.deltaTime);
+                    StoryPlane.transform.position = Vector3.Lerp(StoryPlane.transform.position,
+                        _storyPlaneId % 2 == 0 ? _planeOddPos : _planeEvenPos, Time.deltaTime);
+
+                    if (StoryPlane.transform.localScale.x >= 0.34f)
+                    {
+                        if (_storyPlaneId < 3)
+                        {
+                            _storyPlaneId++;
+
+                            StoryPlane.transform.position = _planeStartPos;
+                            StoryPlane.transform.localScale = new Vector3(0.0001f, 0, 0.0001f);
+
+                            var nextTex = Resources.Load<Texture2D>("Textures/Story/Story" + _storyPlaneId);
+                            StoryPlane.GetComponent<Renderer>().material.mainTexture = nextTex;
+
+                            _outroState = OutroState.Story;
+                        }
+                        else
+                            _outroState = OutroState.Transport2;
+                    }
+                }
+                else
+                {
+                    StoryPlane.transform.localPosition = Vector3.Lerp(StoryPlane.transform.localPosition,
+                        _tempTargetPos, Time.deltaTime * 2);
+
+                    var dist = Vector3.Distance(StoryPlane.transform.localPosition, _tempTargetPos);
+                    if (dist < 0.001f)
+                        _tempTargetPos = new Vector3(Random.Range(-0.01f, 0.01f), Random.Range(-0.01f, 0.01f), _planeStartPos.z);
+                }
+
+                break;
+
+            case OutroState.Transport2:
+                Plane.transform.Rotate(new Vector3(0, 1, 0), _rotSpeed * Time.deltaTime);
+
+                _twirlScriptLeft.angle += AngleIncrease*Time.deltaTime;
+                _twirlScriptRight.angle += AngleIncrease * Time.deltaTime;
+
+                if (_cccScriptLeft.saturation >= 0)
+                {
+                    _cccScriptLeft.saturation -= SaturationDecrease * Time.deltaTime;
+                    _cccScriptRight.saturation -= SaturationDecrease * Time.deltaTime;
+                }
+                else
+                {
+                    if (!_grayScriptLeft.enabled)
+                    {
+                        _grayScriptLeft.enabled = true;
+                        _grayScriptRight.enabled = true;
+                    }
+
+                    _grayScriptLeft.rampOffset += RampIncrease * Time.deltaTime;
+                    _grayScriptRight.rampOffset += RampIncrease * Time.deltaTime;
+                }
+
+                if (_grayScriptLeft.rampOffset > 1.0f)
+                {
+                    _outroState = OutroState.RealMode;
+                    CalibRect.SetActive(true);
+                    Planes.SetActive(false);
+                    Config.ResumeOpenCV();
+                }
+
+                break;
+     
+            case OutroState.RealMode:
+                Plane.transform.rotation = Quaternion.Euler(270, 0, 0);
+
+                if (Config.CamDataUpdate > 0 && Config.CamDataUpdate > _lastDataCheck)
+                {
+                    _lastDataCheck = Config.CamDataUpdate;
+
+                    if (Config.CamData.Length > 0)
+                    {
+                        _webcamTex.LoadRawTextureData(Config.CamData);
+                        _webcamTex.Apply();
+
+                        WebcamTex.texture = _webcamTex;
+                        _planeRenderer.material.mainTexture = _webcamTex;
+                    }
+                }
+
+                if (_grayScriptLeft.rampOffset >= 0)
+                {
+                    _grayScriptLeft.rampOffset -= RampIncrease * Time.deltaTime;
+                    _grayScriptRight.rampOffset -= RampIncrease * Time.deltaTime;
+                }
+                else
+                {
+                    if (_grayScriptLeft.enabled)
+                    {
+                        _grayScriptLeft.enabled = false;
+                        _grayScriptRight.enabled = false;
+                    }
+
+                    if (_cccScriptLeft.saturation < 0.4f)
+                    {
+                        _cccScriptLeft.saturation += SaturationDecrease * Time.deltaTime;
+                        _cccScriptRight.saturation += SaturationDecrease * Time.deltaTime;
+                    }
+                    else
+                    {
+                        _cccScriptLeft.saturation = 0.45f;
+                        _cccScriptRight.saturation = 0.45f;
+                    }
+
+                    if (_twirlScriptLeft.angle - AngleIncrease * Time.deltaTime > 0)
+                    {
+                        _twirlScriptLeft.angle -= AngleIncrease * Time.deltaTime;
+                        _twirlScriptRight.angle -= AngleIncrease * Time.deltaTime;
+                    }
+                    else
+                    {
+                        _twirlScriptLeft.enabled = false;
+                        _twirlScriptRight.enabled = false;
+                    }
+                }
+
+                break;
+        }
+    }
+}
--- Assets/Scripts/World.cs
+++ Assets/Scripts/World.cs
@@ -2,78 +2,159 @@
 
 public class World : MonoBehaviour
 {
-    private GameObject[] sides = new GameObject[6];
+    private readonly GameObject[] _sides = new GameObject[6];
+
+    private int _rotationStep;
+    private Vector3 _rotatePoint;
+    private const float OpenSpeed = 10f;
 
     private InputGamepad inputGamepad;
+
+    public bool IsWorldOpen;
+    public bool IsWorldOpening;
 
 	// Use this for initialization
     private void Start()
     {
         Config.World = this.gameObject;
 
-        inputGamepad = GameObject.Find("CardboardMain").GetComponent<InputGamepad>();
+        var cardMain = GameObject.Find("CardboardMain");
+        if (cardMain != null)
+            inputGamepad = cardMain.GetComponent<InputGamepad>();
 
-        sides[0] = Instantiate(Resources.Load("Prefabs/Gebirge") as GameObject);
-        sides[0].transform.parent = this.transform;
-        sides[0].name = "Gebirge";
+        _sides[0] = Instantiate(Resources.Load("Prefabs/Gebirge") as GameObject);
+        _sides[0].transform.parent = this.transform;
+        _sides[0].name = "Gebirge";
 
-        sides[1] = Instantiate(Resources.Load("Prefabs/Dorf") as GameObject);
-        sides[1].transform.parent = this.transform;
-        sides[1].name = "Dorf";
+        _sides[1] = Instantiate(Resources.Load("Prefabs/Dorf") as GameObject);
+        _sides[1].transform.parent = this.transform;
+        _sides[1].name = "Dorf";
 
-        sides[2] = Instantiate(Resources.Load("Prefabs/Eis") as GameObject);
-        sides[2].transform.parent = this.transform;
-        sides[2].name = "Eis";
+        _sides[2] = Instantiate(Resources.Load("Prefabs/Eis") as GameObject);
+        _sides[2].transform.parent = this.transform;
+        _sides[2].name = "Eis";
 
-        sides[3] = Instantiate(Resources.Load("Prefabs/Feuer") as GameObject);
-        sides[3].transform.parent = this.transform;
-        sides[3].name = "Feuer";
+        _sides[3] = Instantiate(Resources.Load("Prefabs/Feuer") as GameObject);
+        _sides[3].transform.parent = this.transform;
+        _sides[3].name = "Feuer";
 
-        sides[4] = Instantiate(Resources.Load("Prefabs/Wueste") as GameObject);
-        sides[4].transform.parent = this.transform;
-        sides[4].name = "Wueste";
+        _sides[4] = Instantiate(Resources.Load("Prefabs/Wueste") as GameObject);
+        _sides[4].transform.parent = this.transform;
+        _sides[4].name = "Wueste";
 
-        sides[5] = Instantiate(Resources.Load("Prefabs/Wald") as GameObject);
-        sides[5].transform.parent = this.transform;
-        sides[5].name = "Wald";
+        _sides[5] = Instantiate(Resources.Load("Prefabs/Wald") as GameObject);
+        _sides[5].transform.parent = this.transform;
+        _sides[5].name = "Wald";
 
-        for (int i = 0; i < sides.Length; i++)
+        for (int i = 0; i < _sides.Length; i++)
         {
-            if (sides[i] == null)
+            if (_sides[i] == null)
             {
-                sides[i] = Instantiate(Resources.Load("Prefabs/DummySide" + (i)) as GameObject);
-                sides[i].transform.parent = this.transform;
-                sides[i].name = "Side" + i;
+                _sides[i] = Instantiate(Resources.Load("Prefabs/DummySide" + (i)) as GameObject);
+                _sides[i].transform.parent = this.transform;
+                _sides[i].name = "Side" + i;
             }
         }
 
         //Front Side
-        sides[0].transform.position = new Vector3(-9.8f, -27, 67.7f);
-        sides[0].transform.Rotate(new Vector3(-90, 0, 0));
+        _sides[0].transform.position = new Vector3(-9.8f, -27, 67.7f);
+        _sides[0].transform.Rotate(new Vector3(-90, 0, 0));
 
         //Back Side
-        sides[1].transform.position = new Vector3(0, 0, -100);
-        sides[1].transform.Rotate(new Vector3(90, 0, 0));
+        _sides[1].transform.position = new Vector3(0, 0, -100);
+        _sides[1].transform.Rotate(new Vector3(90, 0, 0));
 
         //Right
-        sides[2].transform.position = new Vector3(100, 0, 0);
-        sides[2].transform.Rotate(new Vector3(0, 0, 90));
+        _sides[2].transform.position = new Vector3(100, 0, 0);
+        _sides[2].transform.Rotate(new Vector3(0, 0, 90));
 
         //Left
-        sides[3].transform.position = new Vector3(-150, -6, 176);
-        sides[3].transform.Rotate(new Vector3(0, 0, -90));
+        _sides[3].transform.position = new Vector3(-150, -6, 176);
+        _sides[3].transform.Rotate(new Vector3(0, 0, -90));
 
         //Top
-        sides[4].transform.position = new Vector3(0, 100, 0);
-        sides[4].transform.Rotate(180, 0, 0);
+        _sides[4].transform.position = new Vector3(0, 100, 0);
+        _sides[4].transform.Rotate(180, 0, 0);
 
         //Bottom
-        sides[5].transform.position = new Vector3(0, -100, 0);
+        _sides[5].transform.position = new Vector3(0, -100, 0);
+
+        // Aufklappen
+        _rotatePoint = _sides[4].transform.position + 100*Vector3.forward;
+        _rotationStep = 0;
+
+        IsWorldOpen = false;
+        IsWorldOpening = false;
     }
 
     // Update is called once per frame
-	void Update () {
-//        this.transform.Rotate(inputGamepad.JoystickAxes["LS Y"], inputGamepad.JoystickAxes["LS X"], 0);
-    
+    private void Update()
+    {
+//      this.transform.Rotate(inputGamepad.JoystickAxes["LS Y"], inputGamepad.JoystickAxes["LS X"], 0);
+
+        switch (_rotationStep)
+        {
+            case 1:
+                _sides[4].transform.RotateAround(_rotatePoint, new Vector3(1, 0, 0), OpenSpeed * Time.deltaTime);
+
+                if (_sides[4].transform.rotation.eulerAngles.x <= 270)
+                {
+                    _rotationStep++;
+                    _rotatePoint = _sides[0].transform.position - 72 * Vector3.up + 30 * Vector3.forward;
+                }
+
+                break;
+
+            case 2:
+                _sides[0].transform.RotateAround(_rotatePoint, new Vector3(1, 0, 0), OpenSpeed * Time.deltaTime);
+                _sides[4].transform.RotateAround(_rotatePoint, new Vector3(1, 0, 0), OpenSpeed * Time.deltaTime);
+
+                if (_sides[4].transform.rotation.eulerAngles.x < 10)
+                {
+                    _rotationStep++;
+                    _rotatePoint = _sides[2].transform.position - 100 * Vector3.up;
+                }           
+
+                break;
+
+            case 3:
+                _sides[2].transform.RotateAround(_rotatePoint, new Vector3(0, 0, 1), -OpenSpeed * Time.deltaTime);
+
+                if (_sides[2].transform.rotation.eulerAngles.z > 300)
+                {
+                    _rotationStep++;
+                    _rotatePoint = _sides[3].transform.position - 92 * Vector3.up - 50 * Vector3.left;
+                }
+
+                break;
+
+            case 4:
+                _sides[3].transform.RotateAround(_rotatePoint, new Vector3(0, 0, 1), OpenSpeed * Time.deltaTime);
+
+                if (_sides[3].transform.rotation.eulerAngles.z < 10)
+                {
+                    _rotationStep++;
+                    _rotatePoint = _sides[1].transform.position - 100 * Vector3.up;
+                }
+
+                break;
+
+            case 5:
+                _sides[1].transform.RotateAround(_rotatePoint, new Vector3(1, 0, 0), -OpenSpeed * Time.deltaTime);
+
+                if (_sides[1].transform.rotation.eulerAngles.x > 300)
+                {
+                    _rotationStep++;
+                    IsWorldOpen = true;
+                }
+
+                break;
+        }
+    }
+
+    public void OpenWorld()
+    {
+        IsWorldOpening = true;
+        _rotationStep = 1;
     }
 }
